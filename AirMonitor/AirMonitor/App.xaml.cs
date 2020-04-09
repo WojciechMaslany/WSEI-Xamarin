a4e97671b09288cff675d90072c96b5c66532134
Wojtek <wojciech.maslany1@gmail.com>
2020-04-09 13:46:19 +0200

    Lab2 progress

--- AirMonitor/AirMonitor/App.xaml.cs
+++ AirMonitor/AirMonitor/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AirMonitor
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage (new MyTabbedPage());
         }
 
         protected override void OnStart()
--- AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
+++ AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
@@ -11,16 +11,18 @@ namespace AirMonitor.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class DetailsPage : ContentPage
     {
-        public MainPage()
+        public DetailsPage()
         {
             InitializeComponent();
         }
 
         private void Help_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Co to jest CAQI?", "Lorem ipsum.", "Zamknij");
+            DisplayAlert("Co to jest CAQI?", "The Common Air Quality Index (CAQI) is an air quality index used in Europe since 2006." +
+                " An air quality index (AQI) is used by government agencies to communicate" +
+                " to the public how polluted the air currently is or how polluted it is forecast to become.", "Zamknij");
         }
     }
 }
